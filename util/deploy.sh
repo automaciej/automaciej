82e035da08a7c3a7910cee8da446e6a68e7edf38
Maciej Bliziński <maciej.blizinski@gmail.com>
2017-10-22 13:54:42 +0100

    Dodany własny plik ze stylami.

--- util/deploy.sh
+++ util/deploy.sh
@@ -14,10 +14,13 @@ case "${1:-}" in
     umask 022
     find static -type f -exec chmod 644 {} \;
     find static -type d -exec chmod 755 {} \;
+    echo "Cleaning the destination directory…"
     hugo --cleanDestinationDir
     find public -type f -exec chmod 644 {} \;
     find public -type d -exec chmod 755 {} \;
+    echo "Running rsync…"
     rsync -a --delete public/ "${DEST}"
+    echo "Done."
     ;;
   test)
     hugo -w -D -F --baseUrl="${HOSTNAME}" server
