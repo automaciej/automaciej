b07677648fd7b220be17dac48a0ff58a6ced1f3f
Maciej Bliziński <maciej.blizinski@gmail.com>
2022-09-06 09:25:59 +0100

    Add a shortcut to generate site preview.

--- util/deploy.sh
+++ util/deploy.sh
@@ -5,24 +5,42 @@ set -u
 
 DEST=atemoia:www/blizin.ski
 
-export PATH=$PATH:/home/maciej/src/go/bin
+if [[ -r "private.cfg.sh" ]]; then
+	# To set PREVIEW_DEST and PREVIEW_URL
+	source private.cfg.sh
+fi
+
+PATH=$PATH:/home/maciej/src/go/bin
+export PATH
+
 hugo=hugo
+readonly hugo
 
 helpmsg() {
-  echo "$0 [ deploy | test ]"
+  echo "$0 [ deploy | draft | test ]"
 }
 
-case "${1:-}" in
-  deploy)
+function build {
     umask 022
     find static -type f -exec chmod 644 {} \;
     find static -type d -exec chmod 755 {} \;
     echo "Cleaning the destination directory…"
-    ${hugo} --cleanDestinationDir
+    ${hugo} --cleanDestinationDir "$@"
     find public -type f -exec chmod 644 {} \;
     find public -type d -exec chmod 755 {} \;
+}
+
+case "${1:-}" in
+  deploy)
+    build
     echo "Running rsync…"
     rsync -a --delete public/ "${DEST}"
+    echo "Done."
+    ;;
+  preview)
+    build -D --baseURL "${PREVIEW_URL?}"
+    echo "Running rsync…"
+    rsync -a --delete public/ "${PREVIEW_DEST?}"
     echo "Done."
     ;;
   test)
